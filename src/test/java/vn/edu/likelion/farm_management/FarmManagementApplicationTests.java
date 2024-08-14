8adf0ee5a1ea048fbd7bc6583122cfb4b2e02c99
Nguyen Trong Phuc <93772238+trongphuc0401@users.noreply.github.com>
2024-08-14 10:12:54 +0700

    add project

---
+++ src/test/java/vn/edu/likelion/farm_management/FarmManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.likelion.farm_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
