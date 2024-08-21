79d525b534e968795b50ef87fe72133304e0e8f3
Nguyen Trong Phuc <93772238+trongphuc0401@users.noreply.github.com>
2024-08-21 07:53:02 +0700

    update file by APIDog

---
+++ src/main/java/vn/edu/likelion/farm_management/entity/HarvestEntity.java
@@ -0,0 +1,53 @@
+package vn.edu.likelion.farm_management.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * HarvestEntity -
+ *
+ * @param
+ * @return
+ * @throws
+ */
+@Entity
+@Table(name = "tbl_harvest")
+@Getter
+@Setter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class HarvestEntity extends BaseEntity {
+
+    @Column
+    String plantId; // ID của cây trồng
+
+    @Column
+    String plantName; // Tên cây trồng
+
+    @Column
+    String typePlantId; // ID loại cây trồng
+
+    @Column
+    String farmId; // ID nông trại
+
+    @Column
+    String farmName; // Tên nông trại
+
+    @Column
+    String description; // Mô tả cây trồng
+
+    @Column
+    Double yieldCurrently; // Sản lượng hiện tại
+
+    @Column
+    Double priceCurrently; // Giá hiện tại
+
+    @Column
+    int isDeleted;
+}
