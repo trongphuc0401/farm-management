36690be5057ea2dcdcdb9d89a2379de4dbc159ba
Nguyen Trong Phuc <93772238+trongphuc0401@users.noreply.github.com>
2024-08-26 13:59:43 +0700

    add

--- src/main/java/vn/edu/likelion/farm_management/repository/UserRepository.java
+++ src/main/java/vn/edu/likelion/farm_management/repository/UserRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.edu.likelion.farm_management.entity.UserEntity;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,String> {
+
+    Optional<UserEntity> findByUsername(String username);
 }
